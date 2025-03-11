37ce199d93bbc70ba2a2f572113e4da557b7bda0
“Santosh <developer.santoshk@gmail.com>
2025-03-11 22:27:23 +0530

    update nextjs config to show s3 images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "leadapibucket.s3.ap-south-1.amazonaws.com",
+      },
+    ],
     domains: [
       "images.unsplash.com",
       "plus.unsplash.com",
