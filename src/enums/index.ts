35eeb09200e2b5f49fdf3ae94fb24acc6b1be10c
“Santosh <developer.santoshk@gmail.com>
2025-04-28 21:19:17 +0530

    Update api url

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -4,7 +4,7 @@ export const ROLE_OPERATOR = "ROLE_OPERATOR";
 export const ROLE_SUBSCRIBER = "ROLE_USER";
 
 export const API_BASE_URL =
-  "http://ec2-13-203-193-11.ap-south-1.compute.amazonaws.com:8080/leadapi";
+  "http://ec2-13-201-100-13.ap-south-1.compute.amazonaws.com:8080/leadapi";
 export const BUSINESS_LOGO = "LOGO";
 export const BUSINESS_COVER_PHOTO = "COVER_IMAGE";
 export const BUSINESS_GALLERY = "GALLERY_IMAGE";
